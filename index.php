3e3a64edaa7e3d956a44ec7d2b5b78d97b59fbeb
Kelly97 <Kelly.marie.aguilar@gmail.com>
2017-02-15 21:45:27 -0600

    Primer Avance

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Spotifly</title>
+    <link rel="icon" href="images/favicon.png">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!--Barra de navegación-->
+    <header id="js-navbar" class="navbar navbar-default navbar-fixed-top navbar-semi-transparent navbar-highlight-aquamarine" style="background-color: rgba(46, 11, 76, 0.7);" role="banner">
+      <div class="container">
+
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="sidepanel" data-target="#navbar-nav">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          
+          
+          <a class="navbar-brand" href="#">
+            <span class="navbar-logo"><img src="images/logo.svg" width="100px"></span>
+          </a>
+        </div>
+
+        <nav class="collapse navbar-collapse" id="navbar-nav" role="navigation">  
+
+          <ul class="nav navbar-nav navbar-right nav-main" style="padding-top: 9px;">
+            <li >
+              <a href="#">
+                Premium
+              </a>
+            </li>
+
+            
+            <li >
+              <a href="#">
+                Ayuda
+              </a>
+            </li>
+
+            <li >
+              <a href="#">
+                Descargar
+              </a>
+            </li>
+            
+            <li role="separator" class="divider" style="padding-top:12px; color: #ffffff;">|</li>  
+                
+            <li>
+              <a href="#">
+                Registrarse
+              </a>
+            </li>
+
+            <li>
+              <a  href="#">
+                Iniciar Seción
+              </a>
+            </li>
+            </ul>
+          </nav>
+        </div>
+  </header>
+
+  <!--Carrusel-->
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active" style="margin-top: 35px; margin-bottom: 35px;">
+      <h1 style="color:#ffffff; font-size: 50px; font-weight: bold; text-align: center;">
+        Música para todos.
+      </h1>      
+      <div class="carousel-caption">        
+      </div>
+    </div>
+
+    <div class="item">
+      <h1 style="color:#ffffff; font-size: 50px; font-weight: bold; text-align: center;">
+        Premium para toda tu familia.<br>Por solo 8.99&nbsp;USD.
+      </h1>      
+      <div class="carousel-caption">        
+      </div>
+    </div>
+    <div class="item">
+      <h1 style="color:#ffffff; font-size: 50px; font-weight: bold; text-align: center;">
+        Escucha Spotify en tu PlayStation®.
+      </h1>      
+      <div class="carousel-caption">        
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+    <!--Cuerpo de la página-->
+    <div class="container-fluid">
+      <div class="row">
+          
+      </div>
+      
+    </div>
+  </body>
+</html>
